7b6daa85026e7c1c0a6b8e7455b5302dc8d9efa7
daniel robinson <daniel.robinson.open@gmail.com>
2018-05-02 16:33:21 +0100

    init

---
+++ panic.go
@@ -0,0 +1,75 @@
+package panic
+
+import (
+	"fmt"
+	"sync"
+	"runtime/debug"
+)
+
+// If i is not nil, then panic with value i
+func If(i interface{}) {
+	if i != nil {
+		panic(i)
+	}
+}
+
+// Runs f in a go routine, if a panic happens r will be passed the value from calling recover
+func SafeGo(f func(), r func(i interface{})) {
+	if f == nil {
+		panic(fmt.Errorf("f must be none nil go routine func"))
+	}
+	if r == nil {
+		panic(fmt.Errorf("r must be none nil recover func"))
+	}
+	go func(){
+		defer func(){
+			if rVal := recover(); rVal != nil {
+				r(rVal)
+			}
+		}()
+		f()
+	}()
+}
+
+// Runs each of fs in its own go routine, waits for them all to complete and panics with a collection of all the recover
+// values if there are any
+func SafeGoGroup(fs ...func()) {
+	if len(fs) < 2 {
+		panic(fmt.Errorf("fs must be 2 or more funcs"))
+	}
+	wg := &sync.WaitGroup{}
+	errsMtx := &sync.Mutex{}
+	errs := make([]*Error, 0, len(fs))
+	for _, f := range fs {
+		wg.Add(1)
+		go func(){
+			defer func(){
+				defer wg.Done()
+				if rVal := recover(); rVal != nil {
+					errsMtx.Lock()
+					defer errsMtx.Unlock()
+					errs = append(errs, &Error{
+						Stack: string(debug.Stack()),
+						Value: rVal,
+					})
+				}
+			}()
+			f()
+		}()
+	}
+	wg.Wait()
+	if len(errs) > 0 {
+		panic(&Error{
+			Value: errs,
+		})
+	}
+}
+
+type Error struct{
+	Stack string
+	Value interface{}
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("%v\n%s", e.Value, e.Stack)
+}
---
+++ readme.md
@@ -0,0 +1,59 @@
+panic
+=====
+
+A small util pkg to help with common panic things
+
+If
+==
+
+Syntax sugar
+
+```go
+//old:
+if err := doStuff(); err != nil {
+ panic(err)
+}
+//new:
+panic.If(doStuff())
+```
+
+SafeGo
+======
+
+Prevents your application from crashing if you are running a go routine and a panic occurs in the go routine but there
+is no deferred `recover` call
+
+```go
+//crash entire application
+go func(){
+    panic("uh oh")
+}()
+
+//application continues as normal
+// non blocking
+panic.SafeGo(func(){
+    panic("uh oh")
+}(), func(i interface{}) {
+    // i == "uh oh"
+})
+```
+
+SafeGoGroup
+===========
+
+Runs a collection of routines in a wait group and panics with a collection of panicked values
+
+```go
+defer func(){
+    r := recover()
+    // r contains each of the panic values and stack traces for each
+}()
+//blocking call but safe
+panic.SafeGoGroup(func(){
+    panic(1)
+}(),func(){
+    panic(2)
+}(),func(){
+    panic(3)
+}())
+```
